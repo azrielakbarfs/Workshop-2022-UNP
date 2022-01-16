ab7999dbea15f9a29dd534048f8451c677407916
Azriel Akbar Firmansyah <azrielakbar380@gmail.com>
2022-01-16 14:46:01 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import "./Assets/App.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="Navbar">Header</div>
+      <br></br>
+      <div className="section">
+        <div className="selection">
+          <div className="card selection-1">1</div>
+          <div className="card selection-2">2</div>
+          <div className="card selection-3">3</div>
+          <div className="card selection-4">4</div>
+          <div className="card selection-5">5</div>
+          <div className="card selection-6">6</div>
+        </div>
+      </div>
+      <br></br>
+      <div className="section-2">
+        <div className="kotak">
+          <div className="kotak-1">1
+          <div className="tuyul-1">
+          <div className="tuyul-2">
+          </div>
+          </div>
+          </div>
+          <div className="kotak 2">2</div>
+          <div className="kotak 3">3</div>
+          </div>
+          </div>
+    </div>
+  );
+}
+
+export default App;
